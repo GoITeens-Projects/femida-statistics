6516040475feb0f6a5dcda212558c2f33740c505
0qsenxx <semen.antipyuk@gmail.com>
2024-09-15 22:09:16 +0300

    add dark vars, common, reset

--- src/index.js
+++ src/index.js
@@ -2,22 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import './reset.css';
+import './common.css';
 import { Provider } from 'react-redux';
 import { store, persistor } from './redux/store';
 import { RouterProvider } from 'react-router-dom';
 import { router } from 'router/router';
 import { PersistGate } from 'redux-persist/integration/react';
 
-ReactDOM.createRoot(document.getElementById('root')).render( <
-    React.StrictMode >
-    <
-    Provider store = { store } >
-    <RouterProvider router = { router } >
-    <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
-         <App /> 
-    </PersistGate>
-    </RouterProvider> 
-    </Provider >
-
-    </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router}>
+        <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </RouterProvider>
+    </Provider>
+  </React.StrictMode>
 );
