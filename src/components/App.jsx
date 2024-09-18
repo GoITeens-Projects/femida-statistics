756d4b13d67371ca9afbad831e7f5ffd3cfb84b4
0qsenxx <semen.antipyuk@gmail.com>
2024-09-18 21:18:07 +0300

    header and top chart

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,75 +4,76 @@ import Login from './Login/Login';
 import { Navigation } from './Navigation/Navigation';
 import { nanoid } from 'nanoid';
 import TopChannels from './Tops/Tops';
+import Header from './Header/Header';
 
 const testTop = [
   {
     userAvatarUrl:
       'https://icon-library.com/images/avatar-icon-images/avatar-icon-images-4.jpg',
-    userName: 'name',
+    userName: 'Імʼя користувача',
     messagesQuantity: 12345,
     id: nanoid(),
   },
   {
     userAvatarUrl:
       'https://icon-library.com/images/avatar-icon-images/avatar-icon-images-4.jpg',
-    userName: 'name',
+    userName: 'Імʼя користувача',
     messagesQuantity: 12345,
     id: nanoid(),
   },
   {
     userAvatarUrl:
       'https://icon-library.com/images/avatar-icon-images/avatar-icon-images-4.jpg',
-    userName: 'name',
+    userName: 'Імʼя користувача',
     messagesQuantity: 12345,
     id: nanoid(),
   },
   {
     userAvatarUrl:
       'https://icon-library.com/images/avatar-icon-images/avatar-icon-images-4.jpg',
-    userName: 'name',
+    userName: 'Імʼя користувача',
     messagesQuantity: 12345,
     id: nanoid(),
   },
   {
     userAvatarUrl:
       'https://icon-library.com/images/avatar-icon-images/avatar-icon-images-4.jpg',
-    userName: 'name',
+    userName: 'Імʼя користувача',
     messagesQuantity: 12345,
     id: nanoid(),
   },
   {
     userAvatarUrl:
       'https://icon-library.com/images/avatar-icon-images/avatar-icon-images-4.jpg',
-    userName: 'name',
+    userName: 'Імʼя користувача',
     messagesQuantity: 12345,
     id: nanoid(),
   },
   {
     userAvatarUrl:
       'https://icon-library.com/images/avatar-icon-images/avatar-icon-images-4.jpg',
-    userName: 'name',
+    userName: 'Імʼя користувача',
     messagesQuantity: 12345,
     id: nanoid(),
   },
   {
     userAvatarUrl:
       'https://icon-library.com/images/avatar-icon-images/avatar-icon-images-4.jpg',
-    userName: 'name',
+    userName: 'Імʼя користувача',
     messagesQuantity: 12345,
     id: nanoid(),
   },
   {
     userAvatarUrl:
       'https://icon-library.com/images/avatar-icon-images/avatar-icon-images-4.jpg',
-    userName: 'name',
+    userName: 'Імʼя користувача',
     messagesQuantity: 12345,
     id: nanoid(),
   },
   {
     userAvatarUrl:
       'https://icon-library.com/images/avatar-icon-images/avatar-icon-images-4.jpg',
-    userName: 'name',
+    userName: 'Імʼя користувача',
     messagesQuantity: 12345,
     id: nanoid(),
   },
@@ -81,6 +82,7 @@ const testTop = [
 export const App = () => {
   return (
     <>
+      <Header />
       <Navigation />
       <Outlet />
       <TopChannels topArr={testTop} />
