035530a0f3c58d7f9f756833cfd64d0b80bd8f0d
Oleksandr1shkin <cmurtimur@gmail.com>
2024-09-18 18:37:35 +0300

    перейменував файл

---
+++ src/pages/Overview.jsx
@@ -0,0 +1,22 @@
+import { ServerMembers } from "components/ServerMembers/ServerMembers"
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { NavLink } from "react-router-dom"
+import { fetchStatistics } from "../redux/statistics/operation";
+
+export const Overview = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        // Виконуємо fetch при завантаженні компонента
+        dispatch(fetchStatistics());
+    }, [dispatch]);
+    return (
+        <>
+
+            <ServerMembers />
+        </>
+
+
+    )
+}
